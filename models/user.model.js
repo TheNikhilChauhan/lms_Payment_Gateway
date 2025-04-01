776bd01dc937a0c2f343f3cbcb6fd3f4f47281cd
Nikhil <ace.nikchauhan@gmail.com>
2025-04-01 20:22:14 +0530

    added user model

---
+++ models/user.model.js
@@ -0,0 +1,110 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      maxLength: [50, "Name cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please provide a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must not be less than 8 characters"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["students", "instructor", "admin"],
+        message: "Please select a valid role",
+      },
+      default: "student",
+    },
+
+    avatar: {
+      type: String,
+      default: "default-avatar.png",
+    },
+    bio: {
+      type: String,
+      maxLength: [200, "Bio cannot exceed 200 characters"],
+    },
+    enrolledCourses: [
+      {
+        course: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Course",
+        },
+        enrolledAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+
+    createdCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    lastActive: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//hashing password
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+//compare password
+userSchema.methods.comparePassword = async (enterPassword) => {
+  return await bcrypt.compare(enterPassword, this.password);
+};
+
+userSchema.methods.getResetPasswordToken = () => {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  crypto.createHash("sha256").update(resetToken).digest("hex");
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000; //10mins
+  return resetToken;
+};
+
+userSchema.methods.updateLastActive = function () {
+  this.lastActive = Date.now();
+  return this.lastActive({ validateBeforeSave: false });
+};
+
+//virtual field for total enrolled courses
+userSchema.virtual("totalEnrolledCourses").get(function () {
+  return this.enrolledCourses.length;
+});
+
+export const User = mongoose.model("User", userSchema);
